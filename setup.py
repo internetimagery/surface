b8bde0994c906377e3cd8e58b490e687d556b6bf
Jason Dixon <jason.dixon.email@gmail.com>
2019-07-22 22:04:09 +1200

    more distro stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
+import os.path
 from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as handle:
+    readme = handle.read()
 
 setup(
     name="surface",
     version="0.0.1a",
     description="Expose and compare representation of a public api. (WIP)",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Jason Dixon",
     url="https://github.com/internetimagery/surface",
     packages=["surface"],
     install_requires=["sigtools"],
+    python_requires=">=2.7,>=3.6",
+    license="MIT",
     entry_points={"console_scripts": ["surface=surface.__main__"]},
 )
