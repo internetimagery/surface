490ec6760cc305e99d73f09c0e395b7fb8d5c13f
Jason Dixon <jason.dixon.email@gmail.com>
2019-09-02 13:35:53 +1200

    actually add the thing

---
+++ tests/test_cli.py
@@ -0,0 +1,39 @@
+import subprocess
+import unittest
+import tempfile
+import shutil
+import stat
+import os
+
+from surface import bump_semantic_version, SemVer
+
+
+class TestRun(unittest.TestCase):
+    def setUp(self):
+        self.temp = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.temp, onerror=self.remove_protected)
+
+    def remove_protected(self, action, name, exc):
+        os.chmod(name, stat.S_IWRITE)
+        os.remove(name)
+
+    def test_dump(self):
+        command = ["surface", "-q", "dump", "--depth", "1", "surface"]
+        subprocess.check_call(command)
+
+    def test_dump_output(self):
+        output = os.path.join(self.temp, "myfile.json")
+        command = ["surface", "-q", "dump", "--depth", "1", "-o", output, "surface"]
+        subprocess.check_call(command)
+
+    def test_dump_git(self):
+        command = ["surface", "-q", "dump", "--depth", "1", "--git", self.temp, "surface"]
+        subprocess.check_call(command)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
